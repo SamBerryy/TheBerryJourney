d0a04d0f76c8d9b28acc755569361562a7fa33d7
Sam Berry <sberry@sidetrade.com>
2023-02-05 17:22:22 +0000

    Second Commit

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -42,6 +42,6 @@ export async function getStaticPaths() {
     const posts = await getPosts();
     return {
         paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-        fallback: falsetrue
+        fallback: true
     }
 }
