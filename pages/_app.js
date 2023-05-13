99c956bb358c20fef25527fff868d5ce6010df67
SIDETRADE\sberry <sberry@sidetrade.com>
2023-05-13 18:42:19 +0200

    Add analystics to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.scss'
 import React, { useEffect, useState } from 'react'
 import { Layout } from '../components'
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
